282b34cc2e78e5fabfbbac107553bd714cbd5aa9
Suleiman Aziz <Suleimanaziz212@gmail.com>
2025-07-09 17:29:23 +0300

    submitData function added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+    const fnData= {
+        name,
+        email
+    }
+    const cb = {
+        method: 'POST',
+        headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify(fnData)
+
+    }
+    return fetch('http://localhost:3000/users', cb).then(response => response.json()).then((data) =>{
+       document.body.append(data.id) 
+    }).catch((error) =>{
+       const errorElm = document.createElement('p');
+       errorElm.textContent = `${error.message}`;
+       document.body.appendChild(errorElm);
+    })
+}
